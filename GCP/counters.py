f46abfd93b51f1e0ef2dea2fb314937bfe1b076e
Charly Rosero <charly.rosero@gmail.com>
2021-10-04 23:58:15 -0500

    Uso de ParDo

---
+++ GCP/counters.py
@@ -0,0 +1,34 @@
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+
+
+p = beam.Pipeline(options=PipelineOptions())
+
+class Printer(beam.DoFn):
+     def process(self,data_item):
+          print (data_item)
+    
+def printer(data_item):
+    print (data_item)
+
+class TypeOf(beam.DoFn):
+    def process(self, data_item):
+        print (type(data_item))
+        
+class GetTotal(beam.DoFn):
+     def process(self, element):
+        # get the total transactions for one item
+        return [(str(element[0]),sum(element[1]))]
+
+data_from_source = (p 
+ | 'ReadMyFile' >> ReadFromText('c:\datasets\BreadBasket_DMS.csv')
+ | 'Splitter using beam.Map' >> beam.Map(lambda record: (record.split(','))[0])
+ | 'Map record to 1' >> beam.Map(lambda record: (record, 1))
+ | 'GroupBy the data' >> beam.GroupByKey()
+ | 'Get the total in each day' >> beam.ParDo(GetTotal())
+ | 'Print the date' >> beam.ParDo(Printer())
+ )
+
+result = p.run()
